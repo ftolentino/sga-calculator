efea7b382b507fcb13b72834d9b4d94ec20416a0
ftolentino <filmer.tolentino@gmail.com>
2022-06-26 20:52:44 -0700

    removed local scoped UI for absent UI logic testing

--- src/index.js
+++ src/index.js
@@ -6,27 +6,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 // Planet Age UI logic
-const planetAgeInterface = () => {
-  const personAge = new PlanetAge();
-  
-  const mercuryAge = personAge.showPlanetAge(39, .24);
-  const venusAge = personAge.showPlanetAge(39, .62);
-  const marsAge = personAge.showPlanetAge(39, 1.88);
-  const jupiterAge = personAge.showPlanetAge(39, 11.86);
-  
-  return `${mercuryAge}, ${venusAge}, ${marsAge}, ${jupiterAge}`;
-};
+const personAge = new PlanetAge();
 
-console.log(planetAgeInterface()); 
+const mercuryAge = personAge.showPlanetAge(39, .24);
+const venusAge = personAge.showPlanetAge(39, .62);
+const marsAge = personAge.showPlanetAge(39, 1.88);
+const jupiterAge = personAge.showPlanetAge(39, 11.86);
+
+console.log(mercuryAge, venusAge, marsAge, jupiterAge); 
 
 // Life Expectancy UI logic
-const lifeExpectInterface = () => {
-  let personLife = new LifeExpectancy();
+let personLife = new LifeExpectancy();
 
-  let mercuryLife = personLife;
+let mercuryLifeLeft = personLife.yearsToLive(mercuryAge);
 
-  return `${mercuryLife}`;
-};
-
-console.log(lifeExpectInterface);
+console.log(mercuryLifeLeft);
 
