ac55ade31dbc76acf830c5520d590b2af81796f6
ftolentino <filmer.tolentino@gmail.com>
2022-06-26 20:53:53 -0700

    Create yearsLeft logic

--- src/js/LifeExpectancy.js
+++ src/js/LifeExpectancy.js
@@ -2,6 +2,10 @@ export default class LifeExpectancy {
   constructor() {
     this.yearsLeft = 0;
   }
-
-
+  
+  yearsToLive(planetAge) {
+    const averagePlanetLife = 100;
+    this.yearsLeft = Math.floor(averagePlanetLife - planetAge);
+    return this.yearsLeft;
+  }
 }
