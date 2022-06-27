40f46ca3cf5d11126e124eec1e43e5fdb156ab68
ftolentino <filmer.tolentino@gmail.com>
2022-06-26 23:40:20 -0700

    Updated temp UI logic in index.js to call on business logic

--- src/index.js
+++ src/index.js
@@ -13,7 +13,11 @@ const venusAge = personAge.showPlanetAge(39, .62);
 const marsAge = personAge.showPlanetAge(39, 1.88);
 const jupiterAge = personAge.showPlanetAge(39, 11.86);
 
-console.log(mercuryAge, venusAge, marsAge, jupiterAge); 
+console.log(mercuryAge, venusAge, marsAge, jupiterAge);
+console.log(`You are ${mercuryAge} years old on Mercury!`);
+console.log(`You are ${venusAge} years old on Venus!`);
+console.log(`You are ${marsAge} years old on Mars!`);
+console.log(`You are ${jupiterAge} years old on Jupiter!`);
 
 // Life Expectancy UI logic
 let personLife = new LifeExpectancy();
@@ -24,4 +28,52 @@ let marsLifeLeft = personLife.yearsToLive(marsAge);
 let jupiterLifeLeft = personLife.yearsToLive(jupiterAge);
 
 console.log(mercuryLifeLeft, venusLifeLeft, marsLifeLeft, jupiterLifeLeft);
+
+//Longevity UI logic
+let mercuryLifeBonus = personLife.longevity(mercuryLifeLeft);
+let venusLifeBonus = personLife.longevity(venusLifeLeft);
+let marsLifeBonus = personLife.longevity(marsLifeLeft);
+let jupiterLifeBonus = personLife.longevity(jupiterLifeLeft);
+
+
+const mercuryLife = () => {
+  if (mercuryLifeLeft < 0) {
+    return `Congrats! You have lived ${mercuryLifeBonus} years past the average planet age of 100 on Mercury!`; 
+  } else if (mercuryLifeLeft >= 0) {
+    return `You have ${mercuryLifeLeft} years left to live on Mercury.`;
+  } 
+};
+
+const venusLife = () => {
+  if (venusLifeLeft < 0) {
+    return `Congrats! You have lived ${venusLifeBonus} years past the average planet age of 100 on Venus!`; 
+  } else if (venusLifeLeft >= 0) {
+    return `You have ${venusLifeLeft} years left to live on Venus.`;
+  }
+};
+
+const marsLife = () => {
+  if (marsLifeLeft < 0) {
+    return `Congrats! You have lived ${marsLifeBonus} years past the average planet age of 100 on Mars!`; 
+  } else if (marsLifeLeft >= 0) {
+    return `You have ${marsLifeLeft} years left to live on Mars.`;
+  }
+};
+
+const jupiterLife = () => {
+  if (jupiterLifeLeft < 0) {
+    return `Congrats! You have lived ${jupiterLifeBonus} years past the average planet age of 100 on Jupiter!`; 
+  } else if (jupiterLifeLeft >= 0) {
+    return `You have ${jupiterLifeLeft} years left to live on Jupiter.`;
+  }
+};
+
+console.log(mercuryLife());
+console.log(venusLife());
+console.log(marsLife());
+console.log(jupiterLife());
+
+
+
+
 
