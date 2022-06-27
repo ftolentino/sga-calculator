2e859b7cbc5f2a67d1c7a5ec6a878811f8081de4
ftolentino <filmer.tolentino@gmail.com>
2022-06-26 23:57:35 -0700

    removed longevity test after refactoring UI logic

--- __tests__/LifeExpectancy.test.js
+++ __tests__/LifeExpectancy.test.js
@@ -15,9 +15,4 @@ describe('LifeExpectancy', () => {
     let personLife = new LifeExpectancy();
     expect(personLife.yearsToLive(162)).toEqual(-62);
   });
-
-  test('longevity method should check yearsLeft if it is a negative number if the planetAge was greater than 100, return positive number to reveal longevity', () => {
-    let personLife = new LifeExpectancy();
-    expect(personLife.longevity(-63)).toEqual(63);
-  });
 });
