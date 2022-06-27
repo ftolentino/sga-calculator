559603af646147929c2377f5bca44464a76a8ceb
ftolentino <filmer.tolentino@gmail.com>
2022-06-27 00:05:12 -0700

    Format indentation and spacing

--- src/js/PlanetAge.js
+++ src/js/PlanetAge.js
@@ -1,12 +1,10 @@
-
 export default class PlanetAge {
   constructor() {
     this.planetAge = 0;
   }
-  
+
   showPlanetAge(age, planetYear) {
     this.planetAge = Math.round(age / planetYear);
-    
     return this.planetAge;
   }
 }
